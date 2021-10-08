f9a770ade00685fec8111b86185c61fb0f01ab96
garretthyena <garrett.hyena@gmail.com>
2021-10-08 13:42:31 +0200

    Clase2

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -7,18 +7,34 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
+    public float playerSpeed;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
+
+    private Vector3 movePlayer;
+    public float gravity = 9.8f;
+    public float fallVelocity;
+    public float jumpForce;
 
     float turnSmoothVelocity;
 
     // Update is called once per frame
     void Update()
     {
+
+        movePlayer = movePlayer * speed;
+        controller.Move(movePlayer * Time.deltaTime);
+        Debug.Log(controller.velocity.magnitude);
+
+        SetGravity();
+
+        PlayerSkills();
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         //we use normalize so we make sure we don't multiply the speed while pressing 2 buttons at the same time
+
 
         //magnitude equals to the length of the vector
         if (direction.magnitude >= 0.1f)
@@ -31,5 +47,31 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+
+    public void PlayerSkills()
+    {
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            fallVelocity = jumpForce;
+            movePlayer.y = fallVelocity;
+        }
+    }
+
+    void SetGravity()
+    {
+        //gravity is negative so it is extracted from the player, making it fall to the ground
+
+        if (controller.isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+        else
+        {
+            fallVelocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+        //this creates falling acceleration
     }
 }
